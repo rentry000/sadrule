fef048bc586d3f9897e425f27202b1a636ddb429
rentry000 <rentry@qq.com>
2025-07-31 23:38:01 +0800

    Update adblock_rule_generator_json19.py

--- adblock_rule_generator_json19.py
+++ adblock_rule_generator_json19.py
@@ -352,7 +352,7 @@ URLS = [
 # 并发处理的线程数
 MAX_WORKERS = 10
 # 输出文件名
-OUTPUT_FILENAME = "adblock_reject.json"
+OUTPUT_FILENAME = "adblock_reject19.json"
 
 
 def _convert_range_to_cidrs(line: str) -> list[str]:
